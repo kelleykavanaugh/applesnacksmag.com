6a23618674aa422cb886a019886cf83965f1f13c
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-09 15:51:07 -0700

    Added navigation to index, changed to h3 for headers

--- wp-content/themes/applesnacks5bootstrap/index.php
+++ wp-content/themes/applesnacks5bootstrap/index.php
@@ -6,12 +6,16 @@
       <div id="category">
         <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?>
       </div><!-- End Category-->
-      <center><h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></center>
+      <center><h4><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4></center>
       <p class="text-right"><small><em>By: <?php the_author_posts_link() ?></em></small></p>
       <?php the_excerpt() ?>
     </div><!-- End Span 4-->
     <?php endwhile; else: ?>
     <p>Sorry, no posts matched your criteria.</p>
     <?php endif; ?><!-- End the Loop -->
+    <!--Start Nav-->
+    <?php previous_posts_link( 'Newer Issue' ); ?>//
+    <?php next_posts_link( 'Older Issue' , $max_pages ); ?>
+    <!--End Nav-->
   </div><!-- End "content" -->
 <?php get_footer(); ?>
