3a434bcaa8d0e8b160b56c6c7cdae1c37ac1dfc5
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-09 11:53:10 -0700

    add wp_header tag to header

--- wp-content/themes/applesnacks5bootstrap/header.php
+++ wp-content/themes/applesnacks5bootstrap/header.php
@@ -21,6 +21,8 @@
       <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
                     <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
                                    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+    <!-- The wordpress head tag -->
+       <?php wp_head(); ?>
   </head>
 
   <body>
