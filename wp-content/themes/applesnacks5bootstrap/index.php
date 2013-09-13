3e179cee13e01234f0d51be493ecb3faaa574bc2
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-13 16:11:33 -0700

    matching category to index

--- wp-content/themes/applesnacks5bootstrap/index.php
+++ wp-content/themes/applesnacks5bootstrap/index.php
@@ -1,15 +1,16 @@
     <?php get_header(); ?>
     <div class="row-fluid">
-      <div class="span3">
+      <div class="span2">
       </div>
-      <div class="span6">
+      <div class="span8">
         <div id="border-bottom">
           <h1><?php the_title(); ?></h1>
         </div><!--/border-bottom-->
         <!-- Start the Loop -->
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h3><?php the_title(); ?></h3>        <!--text-right-->
-        <p style="margin: 0px 0px 0px 0px;" class="text-right"><small><em>
+        <h3><?php the_title(); ?></h3>
+        <!--text-right-->
+        <p class="text-right"><small><em>
           By: <?php the_author_posts_link() ?><br>
           Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?><br>
           <?php the_time('F jS, Y') ?>
@@ -22,12 +23,12 @@
         <p>Sorry, no posts matched your criteria.</p>
         <?php endif; ?>
         <!-- End The Loop -->
-        <div class="pull-left"><small><em><?php previous_posts_link('&raquo; Newer Snacks') ?></em></small>
-        </div>
-        <div class="pull-right"><small><em><?php next_posts_link('Older Snacks &laquo;') ?></small></em>
-        </div>
-      </div><!-- /.span6-->
-      <div id="span3">
-      </div><!--/.span3 -->
+        <!-- newer-older links -->
+        <div class="pull-left"><small><em><?php previous_posts_link('&raquo; Newer Snacks') ?></em></small></div>
+        <div class="pull-right"><small><em><?php next_posts_link('Older Snacks &laquo;') ?></small></em></div>
+        <!--/newer-older links -->
+      </div><!-- /.span8-->
+      <div id="span2">
+      </div><!--/.span2 -->
     </div><!-- /.row-fluid -->
     <?php get_footer(); ?>
