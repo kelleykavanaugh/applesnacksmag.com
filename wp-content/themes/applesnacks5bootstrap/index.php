f6c070a31b166dc413afb7b53bc675d866a6b3ba
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-13 12:17:19 -0700

    style table on index, cleaned up code

--- wp-content/themes/applesnacks5bootstrap/index.php
+++ wp-content/themes/applesnacks5bootstrap/index.php
@@ -7,16 +7,16 @@
       <!-- Start the Loop -->
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <h3><center><?php the_title(); ?></center></h3>
-      <table class="table-condensed">
-        <tr>
-          <td>
-        <p class="text-left"><small><em>From Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></small></em></p>
-          </td>
-          <td>
-        <p class="text-right"><small><em>By: <?php the_author_posts_link() ?></em></small></p>
-          </td>
-        </tr>
-      </table>
+        <table class="table">
+          <tr>
+            <td>
+              <p class="text-left"><small><em>From Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></small></em></p>
+            </td>
+            <td>
+              <p class="text-right"><small><em>By: <?php the_author_posts_link() ?></em></small></p>
+            </td>
+          </tr>
+        </table>
                 <?php the_excerpt() ?>
         <?php endwhile; else: ?>
         <p>Sorry, no posts matched your criteria.</p>
