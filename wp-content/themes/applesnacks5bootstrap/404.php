6373410082685ae5c8ceb267a0e0179d30e53e3a
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-13 14:28:36 -0700

    styled 404

--- wp-content/themes/applesnacks5bootstrap/404.php
+++ wp-content/themes/applesnacks5bootstrap/404.php
@@ -4,6 +4,8 @@
       </div><!--/span2-->
       <div class="span8">
         <h1><center>404 - Not Found</center></h1>
+        <p>Whoops! You've found a hole in Apple Snacks Magazine. I'm sorry, I messed up. Please use the back button, or the navigation bar to find your way home.
+        - <a href="http://applesnacksmag.com/author/kelleykavanaugh/">Kelley Kavanaugh</a>
       </div><!-- /.span8-->
       <div id="span2">
       </div><!--/.span2 -->
