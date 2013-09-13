da70874b9404a0605e5f6d2f4f23d943e7d0798a
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-13 15:21:02 -0700

    styling author page with bootstrap

--- wp-content/themes/applesnacks5bootstrap/columns.php
+++ wp-content/themes/applesnacks5bootstrap/columns.php
@@ -13,7 +13,6 @@
         <?php $categories = get_categories();
         foreach ($categories as $cat) {
           if ($cat->category_parent != 0) {
-            echo '<span style="padding-left:10px;">';
           }
           echo '<a href="'.get_option('home').'/'.'category'.'/'.$cat->category_nicename.'/">'.$cat->cat_name.'</a>';
           if ($cat->category_description != '') {
