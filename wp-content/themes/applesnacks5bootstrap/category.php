1080e565beb1e219900abadd1bd3e555dae8668a
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-13 14:36:15 -0700

    styling archive heirarchy pages

---
+++ wp-content/themes/applesnacks5bootstrap/category.php
@@ -0,0 +1,43 @@
+    <?php get_header(); ?>
+    <div class="row-fluid">
+      <div class="span2">
+      </div><!--/span2-->
+      <div class="span8">
+        <div id="border-bottom">
+          <h1><?php single_cat_title(); ?></h1>
+        </div><!--/border-bottom-->
+        <!-- Start the Loop -->
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h3><center><?php the_title(); ?></center></h3>
+        <table class="table">
+          <tr>
+            <td>
+              <p class="text-left"><small><em>From Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></small></em></p>
+            </td>
+            <td>
+              <p class="text-right"><small><em>By: <?php the_author_posts_link() ?></em></small></p>
+            </td>
+          </tr>
+        </table>
+        <div id="border-bottom">
+          <?php the_excerpt() ?>
+        </div><!--/border-bottom-->
+        <?php endwhile; else: ?>
+        <p>Sorry, no posts matched your criteria.</p>
+        <?php endif; ?>
+        <!-- End The Loop -->
+        <table class="table">
+          <tr>
+            <td>
+              <p class="text-left"><small><em><?php previous_posts_link('&raquo; Newer Snacks') ?></em></small></p>
+            </td>
+            <td>
+              <p class="text-right"><small><em><?php next_posts_link('Older Snacks &laquo;') ?></small></em></p>
+            </td>
+          </tr>
+        </table>
+      </div><!-- /.span6-->
+      <div id="span2">
+      </div><!--/.span2 -->
+    </div><!-- /.row-fluid -->
+    <?php get_footer(); ?>
