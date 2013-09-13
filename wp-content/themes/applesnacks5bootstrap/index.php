4a38d774a72a4fff97f0cc84863ea8a52628b210
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-13 12:06:28 -0700

    styling index with tables

--- wp-content/themes/applesnacks5bootstrap/index.php
+++ wp-content/themes/applesnacks5bootstrap/index.php
@@ -1,35 +1,23 @@
     <?php get_header(); ?>
     <div class="row-fluid">
-      <div class="span2">
-      </div>
-      <div class="span8">
-      <!-- Start the Loop -->
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php the_content() ?>
-        <p class="text-right"><small><em>By: <?php the_author_posts_link() ?></em></small></p>
-      </div><!--/.span12 -->
-      <div class="span2">
-      </div>
-    </div><!--/.row-fluid -->
-    <div class="row-fluid">
       <div class="span3">
       </div>
       <div class="span6">
-      <h1><center><?php the_title(); ?></center></h1>
-      <!-- PHP for category pulls -->
-        <?php $categories = get_categories();
-        foreach ($categories as $cat) {
-          if ($cat->category_parent != 0) {
-            echo '<span style="padding-left:10px;">';
-          }
-          echo '<a href="'.get_option('home').'/'.'category'.'/'.$cat->category_nicename.'/">'.$cat->cat_name.'</a>';
-          if ($cat->category_description != '') {
-            echo ' <br> '.$cat->category_description;
-            echo ' <br> ';
-          }
-          echo '<br />';
-        }
-        ?>
+        <h1>Recently Posted Snacks</h1>
+      <!-- Start the Loop -->
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <h3><center><?php the_title(); ?></center></h3>
+      <table class="table-condensed">
+        <tr>
+          <td>
+        <p class="text-left"><small><em>From Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></small></em></p>
+          </td>
+          <td>
+        <p class="text-right"><small><em>By: <?php the_author_posts_link() ?></em></small></p>
+          </td>
+        </tr>
+      </table>
+                <?php the_excerpt() ?>
         <?php endwhile; else: ?>
         <p>Sorry, no posts matched your criteria.</p>
         <?php endif; ?>
