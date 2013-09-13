f7df72e82549e22c2da4b1c23903de160060727a
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-13 14:25:04 -0700

    added 404 theme file

---
+++ wp-content/themes/applesnacks5bootstrap/404.php
@@ -0,0 +1,11 @@
+    <?php get_header(); ?>
+    <div class="row-fluid">
+      <div class="span2">
+      </div><!--/span2-->
+      <div class="span8">
+        <h1><center>404 - Not Found</center></h1>
+      </div><!-- /.span8-->
+      <div id="span2">
+      </div><!--/.span2 -->
+    </div><!-- /.row-fluid -->
+    <?php get_footer(); ?>
