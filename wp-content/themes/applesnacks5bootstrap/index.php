262e741ac05d053dd3a030d2a16605bd580f7e8b
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-14 12:58:50 -0700

    trying to make wp_query work for 3 columns

--- wp-content/themes/applesnacks5bootstrap/index.php
+++ wp-content/themes/applesnacks5bootstrap/index.php
@@ -1,13 +1,24 @@
     <?php get_header(); ?>
     <div class="row-fluid">
-      <div class="span2">
-      </div><!--/span2-->
-      <div class="span8">
+      <div class="span1">
+      </div><!--/span1-->
+      <div class="span10">
         <div id="border-bottom" class="clearfix">
           <h1><?php the_title(); ?></h1>
         </div><!--/border-bottom-->
+      </div><!--/span9-->
+      <div id="span1">
+      </div><!--/.span1 -->
+    </div><!--/row-fluid-->
+
+    <div class="row-fluid">
+
+      <div class="span4">
+        <!-- The Query First 3 -->
+        <?php $the_query = new WP_Query( array( 'showposts' => 3 ) ); ?>
         <!-- Start the Loop -->
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?><?php while ( $the_query->have_posts() ) { $the_query->the_post(); } ?>
+        <div id="border-bottom" class="clearfix">
         <h3><?php the_title(); ?></h3>
         <!--text-right-->
         <p class="text-right"><small><em>
@@ -16,19 +27,57 @@
           <?php the_time('F jS, Y') ?>
         </em></small></p>
         <!--/text-right-->
-        <div id="border-bottom" class="clearfix">
-          <?php the_excerpt() ?>
+        <?php the_excerpt() ?>
         </div><!--/border-bottom-->
-        <?php endwhile; else: ?>
-        <p>Sorry, no posts matched your criteria.</p>
-        <?php endif; ?>
-        <!-- End The Loop -->
+        <?php wp_reset_postdata(); ?>
+      </div><!-- /.span3-->
+
+      <div class="span4">
+        <!-- The Query First 3 -->
+        <?php $the_query = new WP_Query( array( 'offset' => 3 ) ); ?>
+        <!-- Start the Loop -->
+        <?php while ( $the_query->have_posts() ) { $the_query->the_post(); } ?>
+        <div id="border-bottom" class="clearfix">
+        <h3><?php the_title(); ?></h3>
+        <!--text-right-->
+        <p class="text-right"><small><em>
+          By: <?php the_author_posts_link() ?><br>
+          Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?><br>
+          <?php the_time('F jS, Y') ?>
+        </em></small></p>
+        <!--/text-right-->
+        <?php the_excerpt() ?>
+        </div><!--/border-bottom-->
+        <?php wp_reset_postdata(); ?>
+      </div><!-- /.span3-->
+
+      <div class="span4">
+        <!-- The Query First 3 -->
+        <?php $the_query = new WP_Query( array( 'offset' => 6 ) ); ?>
+        <!-- Start the Loop -->
+        <?php while ( $the_query->have_posts() ) { $the_query->the_post(); } ?>
+        <div id="border-bottom" class="clearfix">
+        <h3><?php the_title(); ?></h3>
+        <!--text-right-->
+        <p class="text-right"><small><em>
+          By: <?php the_author_posts_link() ?><br>
+          Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?><br>
+          <?php the_time('F jS, Y') ?>
+        </em></small></p>
+        <!--/text-right-->
+        <?php the_excerpt() ?>
+        </div><!--/border-bottom-->
+        <?php wp_reset_postdata(); ?>
+      </div><!-- /.span3-->
+
+    </div><!--/row-fluid-->
+
+    <div class="row-fluid">
+      <div class="span12">
         <!-- newer-older links -->
-        <div class="pull-left"><small><em><?php previous_posts_link('&raquo; Newer Snacks') ?></em></small></div>
+        <div class="pull-left" style="clearfix"><small><em><?php previous_posts_link('&raquo; Newer Snacks') ?></em></small></div>
         <div class="pull-right"><small><em><?php next_posts_link('Older Snacks &laquo;') ?></small></em></div>
         <!--/newer-older links -->
-      </div><!-- /.span8-->
-      <div id="span2">
-      </div><!--/.span2 -->
-    </div><!-- /.row-fluid -->
+      </div><!--/span12-->
+    </div><!--/row-fluid-->
     <?php get_footer(); ?>
