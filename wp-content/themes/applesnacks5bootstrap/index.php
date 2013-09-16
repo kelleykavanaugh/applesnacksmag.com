f76e095fb899fbb3ecaae4de6b57de245290b17a
Kelley Kavanaugh <kelleykavanaugh@gmail.com>
2013-09-16 12:11:42 -0700

    styling to match apple snacks

--- wp-content/themes/applesnacks5bootstrap/index.php
+++ wp-content/themes/applesnacks5bootstrap/index.php
@@ -3,31 +3,30 @@
       <div class="span1">
       </div><!--/span1-->
       <div class="span10">
-        <div id="border-bottom" class="clearfix">
           <h1>Friday, <?php echo get_the_date(); ?></h1>
-        </div><!--/border-bottom-->
-      </div><!--/span9-->
+      </div><!--/span10-->
       <div id="span1">
       </div><!--/.span1 -->
     </div><!--/row-fluid-->
-    <div class="row-fluid">
+    <div class="row-fluid">   
 <!-- COLUMN 1 -->
       <div class="span4">
       <!-- Start the Loop -->
       <?php $the_query = new WP_Query( array( 'posts_per_page' => 3, 'offset' => 0 ) ); ?>
       <?php if ( $the_query->have_posts() ) : ?>
       <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-        <div id="border-bottom" class="clearfix">
-        <h3><?php the_title(); ?></h3>
+        <div id="category">
+          <em><?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></em>
+        </div><!-- End Category-->
+        <div id="box-in" class="clearfix"><!--box in-->
+        <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
         <!--text-right-->
         <p class="text-right"><small><em>
           By: <?php the_author_posts_link() ?><br>
-          Column: <?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?><br>
-          <?php the_time('F jS, Y') ?>
         </em></small></p>
         <!--/text-right-->
         <?php the_excerpt() ?>
-        </div><!--/border-bottom-->
+        </div><!--/box-in-->
       <!-- End The Loop -->
       <?php endwhile; ?>
       <?php wp_reset_postdata(); ?>
