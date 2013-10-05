277da860a3a50733f034b460387e43927b043604
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-05 13:32:30 -0700

    added fixed heights for all sizes before phone, rearranged index to pull most recent 9 -single loop moved span inside of loop to create rows instead of columns, boom shaka laka.

--- wp-content/themes/applesnacks5-1/index.php
+++ wp-content/themes/applesnacks5-1/index.php
@@ -6,15 +6,13 @@
     </div><!--/row-fluid-->
     <div class="row-fluid">
 <!-- COLUMN 1 -->
-      <div class="span4">
       <!-- Start the Loop -->
-      <?php $the_query = new WP_Query( array( 'posts_per_page' => 3, 'offset' => 0 ) ); ?>
-      <?php if ( $the_query->have_posts() ) : ?>
-      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="span4">
+        <div id="box-in" class="clearfix"><!--box in-->
         <div id="category">
           <em><?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></em>
         </div><!-- End Category-->
-        <div id="box-in" class="clearfix"><!--box in-->
         <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
         <!--text-right-->
         <p class="text-right"><small><em>
@@ -23,65 +21,14 @@
         <!--/text-right-->
         <?php the_excerpt() ?>
         </div><!--/box-in-->
+      </div><!-- /.span4-->
       <!-- End The Loop -->
       <?php endwhile; ?>
       <?php wp_reset_postdata(); ?>
       <?php else: ?>
       <p>Sorry, no posts matched your criteria.</p>
       <?php endif; ?>
-      </div><!-- /.span4-->
+
 <!--END COLUMN 1 -->
-<!-- COLUMN 2 -->
-      <div class="span4">
-      <!-- Start the Loop -->
-      <?php $the_query = new WP_Query( array( 'posts_per_page' => 3, 'offset' => 3 ) ); ?>
-      <?php if ( $the_query->have_posts() ) : ?>
-      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-        <div id="category">
-          <em><?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></em>
-        </div><!-- End Category-->
-        <div id="box-in" class="clearfix"><!--box in-->
-        <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-        <!--text-right-->
-        <p class="text-right"><small><em>
-          By: <?php the_author_posts_link() ?><br>
-        </em></small></p>
-        <!--/text-right-->
-        <?php the_excerpt() ?>
-        </div><!--/box-in-->
-      <!-- End The Loop -->
-      <?php endwhile; ?>
-      <?php wp_reset_postdata(); ?>
-      <?php else: ?>
-      <p>Sorry, no posts matched your criteria.</p>
-      <?php endif; ?>
-      </div><!-- /.span4-->
-<!--END COLUMN 2 -->
-<!-- COLUMN 3 -->
-      <div class="span4">
-      <!-- Start the Loop -->
-      <?php $the_query = new WP_Query( array( 'posts_per_page' => 3, 'offset' => 6 ) ); ?>
-      <?php if ( $the_query->have_posts() ) : ?>
-      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-        <div id="category">
-          <em><?php $category = get_the_category(); if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; } ?></em>
-        </div><!-- End Category-->
-        <div id="box-in" class="clearfix"><!--box in-->
-        <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-        <!--text-right-->
-        <p class="text-right"><small><em>
-          By: <?php the_author_posts_link() ?><br>
-        </em></small></p>
-        <!--/text-right-->
-        <?php the_excerpt() ?>
-        </div><!--/box-in-->
-      <!-- End The Loop -->
-      <?php endwhile; ?>
-      <?php wp_reset_postdata(); ?>
-      <?php else: ?>
-      <p>Sorry, no posts matched your criteria.</p>
-      <?php endif; ?>
-      </div><!-- /.span4-->
-<!--END COLUMN 3 -->
     </div><!--/row-fluid-->
     <?php get_footer(); ?>
