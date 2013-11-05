2f8beb6a4b824b86999140cb51ad42e454d18c5d
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-11-04 21:09:17 -0800

    initial commit for redesign version 7

---
+++ themes/applesnacks7/404.php
@@ -0,0 +1,15 @@
+    <?php get_header(); ?>
+    <div class="row-fluid">
+      <div class="span2">
+      </div><!--/span2-->
+      <div class="span8">
+        <div id="border-bottom">
+        <h1>404 - Not Found</h1>
+        </div><!--/border-bottom-->
+        <p>Whoops! You've found a hole in Apple Snacks Magazine. I'm sorry, I messed up. Please use the back button, or the navigation bar to find your way home.
+        - <a href="http://applesnacksmag.com/author/kelleykavanaugh/">Kelley Kavanaugh</a>
+      </div><!-- /.span8-->
+      <div id="span2">
+      </div><!--/.span2 -->
+    </div><!-- /.row-fluid -->
+    <?php get_footer(); ?>
